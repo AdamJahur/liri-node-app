3f4d0dd5bf941d2e05cc60307baf5bb04b222203
Adam Jahur <adam_jahur@yahoo.com>
2016-12-10 19:12:57 -0500

    did logic for twitter and made output text function

---
+++ liri.js
@@ -0,0 +1,33 @@
+console.log("LIRI: What can I do for you?");
+var fs = require('fs');
+var request = require('request');
+var spotify = require('spotify');
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+var twit = new Twitter(keys);
+var argument = process.argv[2];
+var value = process.argv[3];
+var dataText = process.argv[4];
+
+//Twitter Parameters
+var params = {
+	"screen_name": "AdamNJahur",
+	"count": 20
+}
+
+//Twitter Logic
+if(argument === "my-tweets"){
+	twit.get('statueses/user_timeline', params, goData);
+	function goData(error, data, response){
+		var tweets = data;
+		for (var i = 0; i < tweets.length; i++) {
+			console.log(tweets[i].text);
+			console.log(tweets[i].created_at);
+		}
+	};
+	outputText();
+}
+
+function outputText(){
+	fs.appendFile('log.text', 'Argument: ' + argument + 'Movie or Song Title: ' + value + '. Movie or Song info: ' + dataText + '.');
+}
